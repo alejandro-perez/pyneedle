de2b8567c85e562ecd1de58763beb1e79334c976
Alejandro Perez <alex.perez-mendez@jisc.ac.uk>
2021-11-23 15:17:17 +0100

    Make use we use GTK 3.0

--- pyneedle/pyneedle.py
+++ pyneedle/pyneedle.py
@@ -23,7 +23,8 @@
 # TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
 # SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 #
-
+import gi
+gi.require_version('Gtk', '3.0')
 from gi.repository import Gtk, Pango, Gio, GdkPixbuf, Gdk, GLib, GObject
 import sys, subprocess, time, os, argparse, html
 
